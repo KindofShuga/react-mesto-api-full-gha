e83d01b1ab2155ad10b2519bc2dd61c605964b49
Alina Trubitsyna <alio9l3@yandex.ru>
2023-04-02 18:15:29 +0300

    add cors handler

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
+const corsHandler = require('./middlewares/corsHandler');
 const router = require('./routes/index');
 const { PORT, DB_ADDRESS } = require('./config');
 const errorHandler = require('./middlewares/errorHandler');
@@ -16,7 +17,13 @@ mongoose.set('strictQuery', false);
 app.use(express.json());
 app.use(helmet());
 app.use(cors({ origin: 'http://mesto.alinat.nomoredomains.work' }));
+app.use(corsHandler);
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
